1ab445c7480641b3e0f64997858f8f4e1b16326d
Serkan AKI <serkanaki95@gmail.com>
2019-02-23 22:40:16 +0300

    Gönderme

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BeniYakala
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTikla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTikla
+            // 
+            this.btnTikla.Location = new System.Drawing.Point(314, 156);
+            this.btnTikla.Name = "btnTikla";
+            this.btnTikla.Size = new System.Drawing.Size(95, 37);
+            this.btnTikla.TabIndex = 0;
+            this.btnTikla.Text = "TIKLA";
+            this.btnTikla.UseVisualStyleBackColor = true;
+            this.btnTikla.Click += new System.EventHandler(this.btnTikla_Click);
+            this.btnTikla.MouseEnter += new System.EventHandler(this.btnTikla_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 455);
+            this.Controls.Add(this.btnTikla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTikla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeniYakala
+{
+    public partial class Form1 : Form
+    {
+        int sayac = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Random rnd = new Random();
+        private void btnTikla_MouseEnter(object sender, EventArgs e)
+        {
+
+            
+            btnTikla.Left = rnd.Next(0, (this.ClientSize.Width - btnTikla.Size.Width));
+            btnTikla.Top  = rnd.Next(0, (this.ClientSize.Height - btnTikla.Size.Height));
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            sayac++;
+            Text = sayac.ToString();
+
+        }
+
+        private void btnTikla_Click(object sender, EventArgs e)
+        {
+           
+            btnTikla.Left = 409;
+            btnTikla.Top= 247;
+            //MessageBox.Show(string.Format("Tebrikler, {0}. denemede kazandınız", sayac));
+            MessageBox.Show($"Tebrikler, {sayac}. denemede kazandınız");
+            sayac=0;
+            Text = sayac.ToString();
+        }
+    }
+}
